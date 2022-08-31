45362231d38697551a75659c2a242cbaea028c14
TTboycrypto <92878922+TTboycrypto@users.noreply.github.com>
2022-08-30 21:17:54 -0500

    Inital Commit: Buildspace Soliidty cohort

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Basis of writing Smart Contract: Read Functions, write functions, and changing a state variable. We will be able to call these functions from our react app!
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("Solidity cohort - Path to getting a web3 career!");
+    }
+
+    function wave() public {
+        totalWaves +=1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,39 @@
+const main = async () => {
+    // grabbing the wallet address of contract owner and randomPerson (contract owner deploys the contract)?
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // Compiles the contract and generates necessary files within artifacts directory.
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    // Hardhat creating local ETH network just for this contract. After script completes Hardhat will destroy that local network. New network each time it conract is ran.
+    const waveContract = await waveContractFactory.deploy();
+    // waiting for contract to deploy to local ETH network. Constructor will run when deployed.
+    await waveContract.deployed();
+    // "contract_name"Contract.address gives the address of the deployed contract (Mainnet, testnet, local Blockchain address)
+    console.log("Contract deployed to:", waveContract.address);
+    // To see the address owner of person deploying this contract
+    console.log("COntract deployed by:", owner.address);
+
+    // Manually calling our functions in WavePortal.sol
+    // First calling the function to grab the number of waves
+    // Then doing the wave. Finally, grabbing the waveCount one more time to see if it changed.
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+    
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+    
+    waveCount = await waveContract.getTotalWaves();
+  };
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0); //exit Node process without error
+    } catch (error) {
+        console.log(error);
+        process.exit(0); // exit Node process while indicating "Uncaught Fatal Exception error"
+    }
+};
+
+runMain();
+
